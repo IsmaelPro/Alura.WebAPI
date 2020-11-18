fe52e5de928f71abb133c4178eb312e316719ffb
2856738 <ismael496@yahoo.com.br>
2020-11-18 14:31:48 -0300

    Configurada segurança da api por Token com JWT

--- Alura.WebAPI.WebApp/Api/LivrosController.cs
+++ Alura.WebAPI.WebApp/Api/LivrosController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Alura.ListaLeitura.Modelos;
 using Alura.ListaLeitura.Persistencia;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Alura.WebAPI.WebApp.Api
 {
+    [Authorize]
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class LivrosController : ControllerBase
     {
         private readonly IRepository<Livro> _repo;
